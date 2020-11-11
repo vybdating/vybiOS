7f1cdbeaff2e564a23f9f6e47d96bbf40229eec3
bright <brightahedor@gmail.com>
2020-11-11 02:28:53 +0100

    Initial Commit

---
+++ Vyb Dating/Vyb_DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vyb_DatingApp.swift
+//  Vyb Dating
+//
+//  Created by Bright Ahedor on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Vyb_DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
